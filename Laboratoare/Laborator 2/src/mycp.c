6affd5c2aa0254a8e5f1a16df96be826537242a2
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-13 10:45:53 +0300

    .

---
+++ Laboratoare/Laborator 2/src/mycp.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#define BUF_SIZE 8192
+int main(int argc, char *argv[], char *envp[]){
+        if(argc != 3){
+            printf("Nu ai introdus 2 argumente");
+            exit(1);
+        }
+
+        int fd_source = open(argv[1], O_RDONLY);
+        if(fd_source< 0){
+            printf("Fisierul %s nu exista", argv[1]);
+            exit(1);
+        }
+
+        int fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if(fd_dest < 0){
+            perror("Eroare la deschiderea fisierului destinatie");
+            exit(1);    
+        }
+
+    char buffer[BUF_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd_source, buffer, BUF_SIZE)) > 0) {
+        if (write(fd_dest, buffer, bytes_read) != bytes_read) {
+            perror("Error writing to destination file");
+            close(fd_source);
+            close(fd_dest);
+            exit(1);
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("Error reading source file");
+    }
+}
